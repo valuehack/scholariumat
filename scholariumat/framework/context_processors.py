69fa025a92183165b65f86bbbd5236624ed78ccd
Merlin <merlin.buczek@gmail.com>
2018-07-20 14:47:01 +0200

    removed menu

--- scholariumat/framework/context_processors.py
+++
@@ -1,8 +0,0 @@
-from framework.models import Menu
-
-
-def menus(request):
-    menus = {}
-    for menu in Menu.objects.all():
-        menus[f'{menu.slug}_menu'] = menu.get_items(request)
-    return menus
