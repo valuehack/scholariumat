99c193612e08f148f130776529eb0e43c7e691e2
Merlin Buczek <merlin.buczek@gmail.com>
2018-12-02 18:54:22 +0100

    Removed mailgun

--- config/settings/production.py
+++ config/settings/production.py
@@ -120,17 +120,6 @@ MANAGERS = [
     ("""scholarium""", 'info@scholarium.at'),
 ]
 
-# Anymail (Mailgun)
-# ------------------------------------------------------------------------------
-# https://anymail.readthedocs.io/en/stable/installation/#installing-anymail
-INSTALLED_APPS += ['anymail']  # noqa F405
-EMAIL_BACKEND = 'anymail.backends.mailgun.EmailBackend'
-# https://anymail.readthedocs.io/en/stable/installation/#anymail-settings-reference
-ANYMAIL = {
-    'MAILGUN_API_KEY': env('MAILGUN_API_KEY'),
-    'MAILGUN_SENDER_DOMAIN': env('MAILGUN_DOMAIN')
-}
-
 # Gunicorn
 # ------------------------------------------------------------------------------
 INSTALLED_APPS += ['gunicorn']  # noqa F405
