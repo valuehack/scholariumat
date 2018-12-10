a2f4be2244b50c5fbd80195f60127ec3b7cf1d94
Ilja Göthel <ilja.goethel@arcor.de>
2018-12-10 13:47:02 +0100

    test von background-Prozessen mit &-hack

--- scholariumat/framework/urls.py
+++ scholariumat/framework/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import HomeView, FaqView, ContactView
+from .views import HomeView, FaqView, ContactView, IljaView
 
 app_name = 'framework'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('fragen', FaqView.as_view(), name='faq'),
     path('Kontakt', ContactView.as_view(), name='contact')
+    path('iljatest', IljaView.as_view(), name='ilja')
 ]
